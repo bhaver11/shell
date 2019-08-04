982e74129d17f50e323a6925ccca34e5b1b0d99a
Bhavesh Yadav <bhaveshy11@gmail.com>
2019-08-04 19:43:49 +0530

    Added dir command -error checking left

--- shell.c
+++ shell.c
@@ -6,12 +6,14 @@ extern char **environ; //pointer to array of pointers to environment strings
 
 char* internalCommands[] = {"clr","dir", "env","quit"}; //internal commands which are executed using c functions
 int commandIndex;
+
 void clear() {
     /*The sequence of special characters '\33[3J\33[H\33[2J' is ascii escape sequence
     It essentially brings the cursor to the top left corner of the screen and clears the screen
     Discovered it by tracing the system calls made by 'clear' command in linux */
     write(1,"\33[3J\33[H\33[2J",11); 
 }
+
 void printEnv() {
     printf("Printing Environment Variables\n");
     for(int i = 0; i < sizeof(*environ)/sizeof(**environ); i++) 
@@ -20,21 +22,28 @@ void printEnv() {
     }
 }
 
-
-int main() {
-    char command[20];
+void main() {
+    char commandLineIp[20];
+    char* command[5];
     char cwd[50];
+    
     while(1) {
         getcwd(cwd,sizeof(cwd));
         printf("Welcome to myshell, Enter a command to execute or ask a question..\n>");
         if(cwd!=NULL)
         printf("%s$",cwd);
-        scanf("%[^\n]",command);
+        scanf("%[^\n]",commandLineIp);
         getchar();
 
+        command[0] = strtok(commandLineIp," ");
+        int i = 0;
+        while(command[i]!= NULL) {
+            i++;
+            command[i] = strtok(NULL, " ");
+        }
         for (commandIndex = 0; commandIndex < sizeof(internalCommands)/sizeof(char*); commandIndex++)
         {
-            if(strcmp(command,internalCommands[commandIndex])==0)
+            if(strcmp(command[0],internalCommands[commandIndex])==0)
                 break;
         }
 
@@ -43,9 +52,16 @@ int main() {
         case 0:
             clear();
             break;
+        case 1:
+            printf("Changing directory to : %s",command[1]);
+            chdir(command[1]);
+            break;
         case 2:
             printEnv();
-            break;        
+            break;
+        case 3:
+            _exit(0);
+            break;
         default:
             break;
         }
